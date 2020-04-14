0d8814f53a964650ece610cc428fd3d31123c4fe
lampardme <lampardme@163.com>
2020-04-14 23:24:00 +0800

    :sparkles: feat - master版本

--- src/pages/Login/RegisterForm.jsx
+++ src/pages/Login/RegisterForm.jsx
@@ -3,10 +3,10 @@
  * @Author: longzhang6
  * @Date: 2020-04-13 22:23:37
  * @LastEditors: longzhang6
- * @LastEditTime: 2020-04-14 00:30:00
+ * @LastEditTime: 2020-04-14 23:23:12
  */
 import React from "react";
-import { Form, Input, Select } from "antd";
+import { Form, Input, Select, Button } from "antd";
 import { inject, observer } from "mobx-react";
 
 const { Option } = Select;
@@ -15,6 +15,7 @@ const { Option } = Select;
 @inject("appStore")
 class RegisterForm extends React.Component {
   state = {};
+  formRef = React.createRef();
 
   onFinish = (values) => {
     console.log("Success:", values);
@@ -37,6 +38,8 @@ class RegisterForm extends React.Component {
     </Form.Item>
   );
 
+  componentDidMount() {}
+
   render() {
     return (
       <div className={this.props.className}>
@@ -44,6 +47,7 @@ class RegisterForm extends React.Component {
         <Form
           onFinish={this.onFinish}
           onFinishFailed={this.onFinishFailed}
+          ref={this.formRef}
           initialValues={{
             prefix: "86",
           }}
@@ -110,6 +114,39 @@ class RegisterForm extends React.Component {
             ]}
           >
             <Input.Password placeholder="请确认登录密码" size="large" />
+          </Form.Item>
+          <Form.Item
+            name="Verification"
+            rules={[
+              {
+                required: true,
+                message: "请输入验证码!",
+              },
+              {
+                pattern: "^[0-9]+$",
+                message: "验证码有误，请重新输入",
+              },
+            ]}
+          >
+            <Input placeholder="请输入验证码" size="large" />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              size="large"
+              disabled={
+                (this.formRef.current &&
+                  !this.formRef.current.isFieldsTouched(true)) ||
+                (this.formRef.current &&
+                  this.formRef.current
+                    .getFieldsError()
+                    .filter(({ errors }) => errors.length).length)
+              }
+              className="register-button"
+            >
+              注册
+            </Button>
           </Form.Item>
         </Form>
       </div>
