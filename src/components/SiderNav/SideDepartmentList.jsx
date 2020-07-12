a52655259b58104706d3d5a67484ba11cfc4cf12
lampardme <lampardme@163.com>
2020-07-12 18:44:39 +0800

    :sparkles: feat - 团队列表更新

--- src/components/SiderNav/SideDepartmentList.jsx
+++ src/components/SiderNav/SideDepartmentList.jsx
@@ -3,11 +3,10 @@
  * @Author: longzhang6
  * @Date: 2020-05-13 22:13:14
  * @LastEditors: longzhang6
- * @LastEditTime: 2020-07-12 16:26:53
+ * @LastEditTime: 2020-07-12 18:44:06
  */
 import React, { useState, useEffect } from 'react'
-import { Menu, Dropdown } from 'antd'
-import { DownOutlined, StopOutlined } from '@ant-design/icons'
+import { Select, message } from 'antd'
 import { useSessionStorage } from 'react-use'
 import { useStore } from '@/hooks/useStore'
 import { switchDepartment } from '@/api/department'
@@ -15,33 +14,21 @@ import { useHistory } from 'react-router-dom'
 import { setCurDepart, getCurDepart } from '@/utils/session'
 import styled from 'styled-components'
 
+const { Option } = Select
+
 const SideDepartmentList = () => {
   const [userOrganizes] = useSessionStorage('user-organizes')
   const { userInfoStore } = useStore()
-  const [selectedKeys, setSelectedKeys] = useState([])
+  const [curValue, setCurValue] = useState('')
   const history = useHistory()
 
   useEffect(() => {
-    setSelectedKeys([getCurDepart()])
-  }, [])
-
-  const selectHandler = item => {
-    let _params = {
-      param: item.key,
-      timestamp: JSON.stringify(new Date().getTime()),
-      token: userInfoStore.token,
-      version: userInfoStore.version
+    let _result
+    if (userOrganizes) {
+      _result = userOrganizes.find(depart => depart.code === getCurDepart())
+      setCurValue(_result.name)
     }
-    switchDepartment(_params)
-      .then(_result => {
-        setCurDepart([item.key])
-        setSelectedKeys([item.key])
-        history.push('/home')
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+  }, [])
 
   const getCurDepartName = () => {
     let _result
@@ -53,77 +40,55 @@ const SideDepartmentList = () => {
     }
   }
 
-  const menu = (
-    <Menu
-      theme="dark"
-      style={{ backgroundColor: 'rgb(46, 85, 130)', borderRadius: '5px' }}
-      onClick={selectHandler}
-      selectedKeys={selectedKeys}
-      defaultSelectedKeys={
-        userOrganizes && userOrganizes.length
-          ? userOrganizes.map(org => {
-              if (org.selected) return org.code
-            })
-          : []
-      }
-    >
-      {userOrganizes &&
-        userOrganizes.length &&
-        userOrganizes.map((organization, idx) => (
-          <Menu.Item
-            key={organization.code}
-            title={organization.name}
-            style={{
-              fontSize: '18px',
-              fontWeight: 'bold',
-              paddingLeft: '16px',
-              paddingBottom: '12px',
-              backgroundColor: 'rgb(46, 85, 130)',
-              width: '260px',
-              maxWidth: '260px',
-              minWidth: '260px',
-              overflow: 'hidden',
-              whiteSpace: 'nowrap',
-              textOverflow: 'ellipsis'
-            }}
-          >
-            {organization.name}
-          </Menu.Item>
-        ))}
-    </Menu>
-  )
+  const handleChange = (value, option) => {
+    let _params = {
+      param: option.key,
+      timestamp: JSON.stringify(new Date().getTime()),
+      token: userInfoStore.token,
+      version: userInfoStore.version
+    }
+    switchDepartment(_params)
+      .then(_result => {
+        setCurDepart([option.key])
+        setCurValue(value)
+        message.success('切换团队成功！')
+        history.push('/home')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
   return (
-    <Dropdown overlay={menu} trigger={['hover']} onClick={e => e.preventDefault()}>
-      <DepartmentCur>
-        <Curpart title={getCurDepartName()}>{getCurDepartName()}</Curpart>
-        {/* <StopOutlined /> */}
-        <DownOutlined />
-      </DepartmentCur>
-    </Dropdown>
+    <DepartmentCur>
+      <Select
+        defaultValue={getCurDepartName()}
+        style={{ width: 180 }}
+        onChange={handleChange}
+        value={curValue}
+      >
+        {userOrganizes &&
+          userOrganizes.length &&
+          userOrganizes.map((organization, idx) => (
+            <Option value={organization.name} key={organization.code}>
+              {organization.name}
+            </Option>
+          ))}
+      </Select>
+    </DepartmentCur>
   )
 }
 
 const DepartmentCur = styled.div`
   height: 32px;
-  padding: 16px;
   margin: 16px 0;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   color: white;
   font-size: 18px;
   font-weight: bold;
   cursor: pointer;
-`
-
-const Curpart = styled.div`
-  width: 110px;
-  max-width: 110px;
-  min-width: 110px;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
 `
 
 export default SideDepartmentList
